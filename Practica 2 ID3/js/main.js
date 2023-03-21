53deca8b0e802ca4d75301fc793f6bb05b951e75
JavierGil5 <javigi01@ucm.es>
2023-03-21 17:24:48 +0100

    carga txt

---
+++ Practica 2 ID3/js/main.js
@@ -0,0 +1,26 @@
+let nombreAtribElem, nombreJuegoElem;
+let nombreAtrib, nombreJuego;
+
+function onPageLoad(){
+    nombreAtribElem = document.getElementById("nomAtrib");
+    nombreJuegoElem = document.getElementById("nomJuego");
+    nombreAtribElem.addEventListener('change', (event)=> readTextFile(event));
+    nombreJuegoElem.addEventListener('change', (event)=> readTextFile(event));
+}
+
+function nombreJuegoChange(){
+    nombreJuego = nombreJuegoElem.value;
+}
+
+function readTextFile(event)
+{
+    const fileList = event.target.files;
+    if(fileList.length > 0){
+         const reader = new FileReader();
+         reader.addEventListener('load', (event) => {
+             const result = event.target.result;
+             console.log(result);
+         })
+         reader.readAsText(fileList[0]);
+    }
+}
